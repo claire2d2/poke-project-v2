72931ed7e6c2610666f9e69be25d5162af7feec7
Claire Song <claireyuan.song@gmail.com>
2024-03-28 14:07:09 +0100

    initialize one pokemon page

--- src/pages/OnePoke.tsx
+++ src/pages/OnePoke.tsx
@@ -1,17 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import pokeApi from "../service/pokeApi";
+
+type PokeImage = {
+  other: {
+    "official-artwork": {
+      front_default: string;
+      front_shiny: string;
+    };
+  };
+};
+
+type PokeData = {
+  id: number;
+  name: string;
+  sprites: PokeImage;
+};
 
 const OnePoke = () => {
   // use state to get the wanted pokemon from the pokemonlist? state
-  const [poke, setPoke] = useState<string>("");
+  const [pokeData, setPokeData] = useState<PokeData | null>(null);
 
   // get the pokemon name from the url
-  const pokeName = useParams();
+  const { pokeId } = useParams();
+
+  // fetch the pokemon using axios
+
+  async function fetchPokeData(name: string) {
+    try {
+      const response = await pokeApi.get<PokeData>(`/pokemon/${name}`);
+      setPokeData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  // use effect to launch the fetch function once mounted
+
+  useEffect(() => {
+    fetchPokeData(pokeId);
+  }, [pokeId]);
+
+  // show "Loading" if the data is loading or nonexistent
+  if (!pokeData) {
+    return <p>Loading</p>;
+  }
 
   return (
     <div>
-      This will be the page where we show the data for one specific pokemon!
+      <h1>{pokeData.name}</h1>
+      <img
+        src={pokeData.sprites.other["official-artwork"].front_default}
+        alt={`official artwork  of ${pokeData.name}`}
+      />
+      <img src={pokeData.sprites.front_default} alt="" />
     </div>
   );
 };
