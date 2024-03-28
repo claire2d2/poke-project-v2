d1b939e0291c403819c7d1ffaf5cf0e217aa4488
Claire Song <claireyuan.song@gmail.com>
2024-03-28 12:29:00 +0100

    structured app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+// import pages
+import Layout from "./pages/Layout";
+import HomePage from "./pages/HomePage";
+import AllPokemon from "./pages/AllPokemon";
+import OnePoke from "./pages/OnePoke";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
-      <div>This will be our app</div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="pokemon">
+            <Route index element={<AllPokemon />} />
+            <Route path=":pokeId" element={<OnePoke />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </>
   );
 }
