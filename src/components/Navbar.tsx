4fe1f98e8e71600a3aa1cf5bcd36f5553674249c
Ivan Stoyanov <ivanstoyanov@outlook.com>
2024-03-28 12:59:32 +0100

    basic navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar absolute top-0 w-full h-75px bg-blue-900 text-white flex items-center">
+      <h1 className="mx-auto">Navbar Here!</h1>
+    </div>
+  );
 };
 
 export default Navbar;
