318899baa2d6ec3572e27aaae35c6fa1a607fd6b
Claire Song <claireyuan.song@gmail.com>
2024-03-28 12:42:02 +0100

    add layout

--- src/pages/OnePoke.tsx
+++ src/pages/OnePoke.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const OnePoke = () => {
+  // use state to get the wanted pokemon from the pokemonlist? state
+  const [poke, setPoke] = useState<string>("");
+
+  // get the pokemon name from the url
+  const pokeName = useParams();
+
   return (
     <div>
       This will be the page where we show the data for one specific pokemon!
