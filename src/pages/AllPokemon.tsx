698bf029b265327ff3ac8a65e3f519b40b51a2c0
Claire Song <claireyuan.song@gmail.com>
2024-03-28 11:50:22 +0100

    added basic pages

---
+++ src/pages/AllPokemon.tsx
@@ -0,0 +1,5 @@
+const AllPokemon = () => {
+  return <div>This will be the page where we show all the pokemon!</div>;
+};
+
+export default AllPokemon;
---
+++ src/pages/OnePoke.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OnePoke = () => {
+  return (
+    <div>
+      This will be the page where we show the data for one specific pokemon!
+    </div>
+  );
+};
+
+export default OnePoke;
