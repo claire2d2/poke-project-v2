40c39b359f93bcee27880305c1cd372fd739e963
smnraphael <simonraphael@hotmail.fr>
2024-03-29 15:56:04 +0100

    made filters automated based on api

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,125 @@
+import { useState, useEffect } from "react";
+import pokeApi from "../service/pokeApi";
+
 type Props = {
   search: string;
   setSearch: (search: string) => void;
 };
 
+// Generation types
+type Generation = {
+  name: string;
+};
+type GenerationData = {
+  results: Array<Generation>;
+};
+
+// Color types
+type Color = {
+  name: string;
+};
+type ColorData = {
+  results: Array<Color>;
+};
+
+// Type types
+type Type = {
+  name: string;
+};
+type TypeData = {
+  results: Array<Type>;
+};
+
 const Sidebar: React.FC<Props> = ({ search, setSearch }) => {
+  const [generation, setGeneration] = useState<Array<Generation>>([]);
+  const [color, setColor] = useState<Array<Color>>([]);
+  const [type, setType] = useState<Array<Type>>([]);
+
+  // Search bar filter
   const handleSearch = (e: React.FormEvent<HTMLInputElement>) => {
     setSearch(e.currentTarget.value);
   };
+
+  // Retrieve generation filters
+  async function fetchGenerationFilters() {
+    try {
+      const { data } = await pokeApi.get<GenerationData>("/generation");
+      setGeneration(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchGenerationFilters();
+  }, []);
+
+  const pokeGeneration = generation.slice(0, 3).map((gen, index) => (
+    <div className="flex gap-2">
+      <input
+        type="checkbox"
+        name={`generation/${index + 1}`}
+        id={`generation/${index + 1}`}
+      />
+      <label key={index} htmlFor={`generation/${index + 1}`}>
+        {gen.name}
+      </label>
+    </div>
+  ));
+
+  // Retrieve color filters
+  async function fetchColorFilters() {
+    try {
+      const { data } = await pokeApi.get<ColorData>("/pokemon-color");
+      setColor(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchColorFilters();
+  });
+
+  const pokeColor = color.map((col, index) => (
+    <div className="flex gap-2">
+      <input
+        type="checkbox"
+        name={`pokemon-color/${index + 1}`}
+        id={`pokemon-color/${index + 1}`}
+      />
+      <label key={index} htmlFor={`pokemon-color/${index + 1}`}>
+        {col.name}
+      </label>
+    </div>
+  ));
+
+  // Retrieve type filters
+  async function fetchTypeFilters() {
+    try {
+      const { data } = await pokeApi.get<TypeData>("/type");
+      setType(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTypeFilters();
+  }, []);
+
+  const pokeType = type.map((typ, index) => (
+    <div className="flex gap-2">
+      <input
+        type="checkbox"
+        name={`type/${index + 1}`}
+        id={`type/${index + 1}`}
+      />
+      <label key={index} htmlFor={`type/${index + 1}`}>
+        {typ.name}
+      </label>
+    </div>
+  ));
 
   return (
     <div className="w-52 bg-blue-200 flex flex-col gap-10 p-2">
@@ -24,136 +137,20 @@ const Sidebar: React.FC<Props> = ({ search, setSearch }) => {
         </label>
       </div>
 
-      {/* TO DO: Automation based on API */}
       <div className="generation flex flex-col">
         <h1 className="text-lg">Generation</h1>
-        <label htmlFor="generation-one">
-          Generation I
-          <input type="checkbox" name="generation-one" id="generation-one" />
-        </label>
-        <label htmlFor="generation-two">
-          Generation II
-          <input type="checkbox" name="generation-two" id="generation-two" />
-        </label>{" "}
-        <label htmlFor="generation-three">
-          Generation III
-          <input
-            type="checkbox"
-            name="generation-three"
-            id="generation-three"
-          />
-        </label>
+        {pokeGeneration}
       </div>
 
       {/* TO DO: Automation based on API */}
       <div className="color flex flex-col">
         <h1 className="text-lg">Color</h1>
-        <label htmlFor="green">
-          Green
-          <input type="checkbox" name="green" id="green" />
-        </label>
-        <label htmlFor="red">
-          Red
-          <input type="checkbox" name="red" id="red" />
-        </label>
-        <label htmlFor="blue">
-          Blue
-          <input type="checkbox" name="blue" id="blue" />
-        </label>
+        {pokeColor}
       </div>
 
-      {/* TO DO: Automation based on API */}
       <div className="type flex flex-col">
         <h1 className="text-lg">Type</h1>
-        <label htmlFor="normal">
-          Normal
-          <input type="checkbox" name="normal" id="normal" />
-        </label>
-
-        <label htmlFor="fire">
-          Fire
-          <input type="checkbox" name="fire" id="fire" />
-        </label>
-
-        <label htmlFor="water">
-          Water
-          <input type="checkbox" name="water" id="water" />
-        </label>
-
-        <label htmlFor="electric">
-          Electric
-          <input type="checkbox" name="electric" id="electric" />
-        </label>
-
-        <label htmlFor="grass">
-          Grass
-          <input type="checkbox" name="grass" id="grass" />
-        </label>
-
-        <label htmlFor="ice">
-          Ice
-          <input type="checkbox" name="ice" id="ice" />
-        </label>
-
-        <label htmlFor="fighting">
-          Fighting
-          <input type="checkbox" name="fighting" id="fighting" />
-        </label>
-
-        <label htmlFor="poison">
-          Poison
-          <input type="checkbox" name="poison" id="poison" />
-        </label>
-
-        <label htmlFor="ground">
-          Ground
-          <input type="checkbox" name="ground" id="ground" />
-        </label>
-
-        <label htmlFor="flying">
-          Flying
-          <input type="checkbox" name="flying" id="flying" />
-        </label>
-
-        <label htmlFor="psychic">
-          Psychic
-          <input type="checkbox" name="psychic" id="psychic" />
-        </label>
-
-        <label htmlFor="bug">
-          Bug
-          <input type="checkbox" name="bug" id="bug" />
-        </label>
-
-        <label htmlFor="rock">
-          Rock
-          <input type="checkbox" name="rock" id="rock" />
-        </label>
-
-        <label htmlFor="ghost">
-          Ghost
-          <input type="checkbox" name="ghost" id="ghost" />
-        </label>
-
-        <label htmlFor="dragon">
-          Dragon
-          <input type="checkbox" name="dragon" id="dragon" />
-        </label>
-
-        <label htmlFor="dark">
-          Dark
-          <input type="checkbox" name="dark" id="dark" />
-        </label>
-
-        <label htmlFor="steel">
-          Steel
-          <input type="checkbox" name="steel" id="steel" />
-        </label>
-
-        <label htmlFor="fairy">
-          Fairy
-          <input type="checkbox" name="fairy" id="fairy" />
-        </label>
+        {pokeType}
       </div>
     </div>
   );
