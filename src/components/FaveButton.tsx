3dcdda477e4ae1d80f81626ccf9f3531d3952647
Ivan Stoyanov <ivanstoyanov@outlook.com>
2024-03-29 15:43:33 +0100

    fav button getting better but needs protection

--- src/components/FaveButton.tsx
+++ src/components/FaveButton.tsx
@@ -4,42 +4,50 @@ import axios from "axios";
 type pokeObject = {
   name: string;
   url: string;
-  favorite: boolean;
+  favorite: Array<pokeFave> | null;
 };
+
+type pokeFave = { pokemonId: number; id: number };
 
 const FaveButton: React.FC<{ pokeId: number }> = ({ pokeId }) => {
   // testing style
 
-  // testing to implement the favorite
+  // state for pokemon data
   const [currentPoke, setCurrentPoke] = useState<pokeObject>({
     name: "",
     url: "",
-    favorite: false,
+    favorite: [],
   });
+
+  // state to determine if pokemon is fave or not
   const [isFave, setIsFave] = useState<boolean | null>(null);
+
+  // state to determine if there is data that is being fetched
+
+  const [isFetchingData, setIfFetchingData] = useState<boolean>(false);
 
   // creating favorites in the concerned pokemon
   async function fetchCurrentPoke() {
     try {
+      setIfFetchingData(true);
       const { data } = await axios.get(
-        `https://poke-backend.adaptable.app/favorite/${pokeId}`
+        `https://poke-backend.adaptable.app/pokemons/${pokeId}?_embed=favorite`
       );
-      console.log(data);
-      if (data) {
+      setIfFetchingData(false);
+      if (data.favorite.length > 0) {
+        setCurrentPoke(data);
         setIsFave(true);
       } else {
         setIsFave(false);
       }
     } catch (error) {
       console.log(error);
-      setIsFave(false);
     }
   }
 
   useEffect(() => {
     fetchCurrentPoke();
-    console.log(isFave);
-  }, []);
+  }, [pokeId]);
 
   // handle make the current pokemon favorite
 
@@ -49,20 +57,25 @@ const FaveButton: React.FC<{ pokeId: number }> = ({ pokeId }) => {
     if (!isFave) {
       console.log("test make fave on click");
       makeFavorite();
+      setIsFave(true);
     } else {
+      console.log("going to make this a fave");
       deleteFavorite();
+      setIsFave(false);
     }
   }
 
   async function makeFavorite() {
     try {
+      setIfFetchingData(true);
       const response = await axios.post(
         `https://poke-backend.adaptable.app/favorite`,
-        { id: pokeId, pokeId: pokeId }
+        { pokemonId: pokeId }
       );
       console.log("I have added a fave");
       console.log(response);
-      setIsFave(true);
+      console.log("fave status after making fave:", isFave);
+      setIfFetchingData(false);
     } catch (error) {
       console.log(error);
     }
@@ -70,20 +83,29 @@ const FaveButton: React.FC<{ pokeId: number }> = ({ pokeId }) => {
 
   async function deleteFavorite() {
     try {
+      setIfFetchingData(true);
+      // Find the index of the favorite object with the matching pokemonId
+      const idToDelete: number = currentPoke.favorite[0].id;
+
+      // Make delete request to delete the favorite
       const response = await axios.delete(
-        `https://poke-backend.adaptable.app/favorite/${pokeId}`
+        `https://poke-backend.adaptable.app/favorite/${idToDelete}`
       );
       console.log("I have deleted a fave");
       console.log(response);
-      setIsFave(false);
+      setIfFetchingData(false);
     } catch (error) {
       console.log(error);
     }
   }
   const buttonStyle = isFave ? "text-green-700" : "text-red-700";
-  //
+
   return (
-    <button onClick={handleFavorite} className={buttonStyle}>
+    <button
+      disabled={isFetchingData ? true : false}
+      onClick={handleFavorite}
+      className={buttonStyle}
+    >
       make {currentPoke.name} fave
     </button>
   );
