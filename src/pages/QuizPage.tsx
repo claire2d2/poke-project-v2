176e4804bea0124cbbc7aed2c8730611e0b9fef4
Ivan Stoyanov <ivanstoyanov@outlook.com>
2024-04-01 16:44:29 +0200

    got only outline to be disoplayed until answer picked, added a dialog with play button

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -2,25 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import whoIsThatPokemonSound from "../../public/who-s-that-pokemon.mp3";
 
-type PokeImage = {
-  other: {
-    "official-artwork": {
-      front_default: string;
-    };
-  };
-};
-
-type PokeType = {
-  type: {
-    name: string;
-  };
-};
-
-type PokemonData = {
-  name: string;
-  sprites: PokeImage;
-  types: PokeType[];
-};
+// Define types
 
 const QuizPage: React.FC = () => {
   const [pokemonList, setPokemonList] = useState<PokemonData[]>([]);
@@ -29,6 +11,9 @@ const QuizPage: React.FC = () => {
   const [feedback, setFeedback] = useState<string>("");
   const [pokemonImage, setPokemonImage] = useState<string>("");
   const [isMuted, setIsMuted] = useState<boolean>(false);
+  const [correctAnswerSelected, setCorrectAnswerSelected] =
+    useState<boolean>(false);
+  const [showModal, setShowModal] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchPokemonList = async () => {
@@ -49,7 +34,7 @@ const QuizPage: React.FC = () => {
   }, [pokemonList]);
 
   useEffect(() => {
-    if (pokemonList.length > 0 && correctAnswer === "") {
+    if (!showModal && pokemonList.length > 0 && correctAnswer === "") {
       const randomIndex = Math.floor(Math.random() * pokemonList.length);
       const randomPokemon = pokemonList[randomIndex];
       setCorrectAnswer(randomPokemon.name);
@@ -64,10 +49,10 @@ const QuizPage: React.FC = () => {
           console.error("Error fetching Pokémon image:", error);
         });
     }
-  }, [pokemonList, correctAnswer]);
+  }, [pokemonList, correctAnswer, showModal]);
 
   useEffect(() => {
-    if (correctAnswer !== "") {
+    if (!showModal && correctAnswer !== "") {
       const incorrectOptions: string[] = [];
       while (incorrectOptions.length < 2) {
         const randomIndex = Math.floor(Math.random() * pokemonList.length);
@@ -83,13 +68,15 @@ const QuizPage: React.FC = () => {
       const shuffledOptions = allOptions.sort(() => Math.random() - 0.5);
       setOptions(shuffledOptions);
     }
-  }, [correctAnswer, pokemonList]);
+  }, [correctAnswer, pokemonList, showModal]);
 
   const handleAnswerSelection = (selectedAnswer: string) => {
     if (selectedAnswer === correctAnswer) {
       setFeedback("Great job! You got it right!");
+      setCorrectAnswerSelected(true);
     } else {
       setFeedback("Uh-oh, better go back to our Pokédex and keep learning!");
+      setCorrectAnswerSelected(false);
     }
   };
 
@@ -102,14 +89,76 @@ const QuizPage: React.FC = () => {
     setOptions([]);
     setFeedback("");
     setPokemonImage("");
+    setCorrectAnswerSelected(false);
+  };
+
+  const handlePlayClick = () => {
+    setShowModal(false);
   };
 
   return (
     <div>
-      {correctAnswer && options.length === 3 && (
+      {showModal && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 transition-opacity"
+              aria-hidden="true"
+            >
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            ></span>
+
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3
+                      className="text-lg leading-6 font-medium text-gray-900"
+                      id="modal-title"
+                    >
+                      Who's that Pokémon?
+                    </h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Click the button below to start playing!
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  onClick={handlePlayClick}
+                  type="button"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
+                >
+                  Play
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {!showModal && (
         <div>
-          <p>Who's that Pokémon?</p>
-          <img src={pokemonImage} alt={correctAnswer} />
+          <p>Who's that Pokémon</p>
+          <img
+            src={pokemonImage}
+            alt={correctAnswer}
+            className="rounded-md shadow-md"
+            style={{
+              filter:
+                correctAnswerSelected || feedback !== ""
+                  ? "none"
+                  : "brightness(0%)",
+            }}
+          />
           <ul>
             {options.map((option, index) => (
               <li key={index} onClick={() => handleAnswerSelection(option)}>
@@ -120,13 +169,12 @@ const QuizPage: React.FC = () => {
           <p>{feedback}</p>
         </div>
       )}
-      {!correctAnswer && <p>Loading...</p>}
-      <div>
-        <button onClick={reloadPokemon}>Reload Pokémon</button>
-      </div>
+
       <audio autoPlay={!isMuted} src={whoIsThatPokemonSound} muted={isMuted} />
+
       <div>
         <button onClick={toggleMute}>{isMuted ? "Unmute" : "Mute"}</button>
+        <button onClick={reloadPokemon}>Reload Pokémon</button>
       </div>
     </div>
   );
