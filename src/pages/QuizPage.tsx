3dbb08a98e6e99cc8b1163e68faae71f6a0b7544
Ivan Stoyanov <ivanstoyanov@outlook.com>
2024-04-02 15:41:43 +0200

    changed modal, improved muting

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -23,8 +23,10 @@ type PokemonData = {
   types: PokeType[];
 };
 
-function playWhoIsThatPokemonSound() {
-  new Audio(whoIsThatPokemonSound).play();
+function playWhoIsThatPokemonSound(isMuted: boolean) {
+  if (!isMuted) {
+    new Audio(whoIsThatPokemonSound).play();
+  }
 }
 
 const QuizPage: React.FC = () => {
@@ -115,58 +117,55 @@ const QuizPage: React.FC = () => {
     setFeedback("");
     setPokemonImage("");
     setCorrectAnswerSelected(false);
-    if (!isMuted) {
-      playWhoIsThatPokemonSound();
-    }
+    playWhoIsThatPokemonSound(isMuted);
   };
 
   const handlePlayClick = () => {
     setShowModal(false);
-    playWhoIsThatPokemonSound();
+    playWhoIsThatPokemonSound(isMuted);
   };
 
   return (
     <div>
       {showModal && (
-        <div className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <div
-              className="fixed inset-0 transition-opacity"
-              aria-hidden="true"
-            >
-              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-            </div>
+        <div
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+          className="fixed z-10 inset-0 bg-cover"
+        >
+          <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center">
+            <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
 
-            <span
-              className="hidden sm:inline-block sm:align-middle sm:h-screen"
-              aria-hidden="true"
-            ></span>
-
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <h3
-                      className="text-lg leading-6 font-medium text-gray-900"
-                      id="modal-title"
-                    >
-                      Who's that Pokémon?
-                    </h3>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        Click the button below to start playing!
-                      </p>
-                    </div>
+            <div className="align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all p-10 flex flex-col gap-5">
+              <div className="bg-white">
+                <div className="text-center">
+                  <h3 className="text-lg font-medium text-gray-900 mb-5">
+                    Who's that Pokémon?!
+                  </h3>
+                  <div className="flex flex-col gap-3 text-center">
+                    <p className="text-sm text-gray-500">
+                      Click the play button below to start playing!
+                    </p>
+                    <p className="text-sm text-gray-500">
+                      (Don't forget to click the mute button if you're not in
+                      the mood for sound)
+                    </p>
                   </div>
                 </div>
               </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <div className="bg-gray-50 flex flex-row justify-center">
                 <button
                   onClick={handlePlayClick}
                   type="button"
                   className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
                 >
                   Play
+                </button>
+                <button
+                  onClick={toggleMute}
+                  type="button"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-500 text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:ml-3 sm:w-auto sm:text-sm"
+                >
+                  {isMuted ? "Unmute" : "Mute"}
                 </button>
               </div>
             </div>
