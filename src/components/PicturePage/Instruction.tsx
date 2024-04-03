5ad62bda9febddd6b497a618e653caa8e74a7516
Claire Song <claireyuan.song@gmail.com>
2024-04-03 21:39:55 +0200

    picture page is more responsive

--- src/components/PicturePage/Instruction.tsx
+++ src/components/PicturePage/Instruction.tsx
@@ -5,7 +5,7 @@ const Instruction: React.FC<{
   children: ReactNode;
 }> = ({ stepName, children }) => {
   return (
-    <div className="relative bg-white h-full w-1/3 rounded-2xl border-8 border-blue-500 drop-shadow-lg">
+    <div className="relative bg-white h-1/2 md:h-full w-full md:w-1/3 rounded-2xl border-4 border-blue-500 drop-shadow-lg">
       <h3 className="text-xl font-bold">{stepName}</h3>
       <div className="flex flex-col flex-wrap justify-center items-center h-full pb-4">
         {children}
