bed8947348eda8438174095d74c0beb857546b89
Claire Song <claireyuan.song@gmail.com>
2024-04-03 14:51:22 +0200

    transfering data from old branch

--- src/components/PicturePage/Instruction.tsx
+++ src/components/PicturePage/Instruction.tsx
@@ -1,15 +1,12 @@
 import React, { ReactNode } from "react";
 
 const Instruction: React.FC<{
-  instrStep: number;
   stepName: string;
   children: ReactNode;
-}> = ({ instrStep, stepName, children }) => {
+}> = ({ stepName, children }) => {
   return (
     <div className="relative bg-white h-full w-1/3 rounded-2xl border-8 border-blue-500 drop-shadow-lg">
-      <div className="absolute -top-10 -left-10 border-8 border-blue-500 flex items-center justify-center  text-6xl font-bold text-red-800 bg-white rounded-full w-1/6 h-1/6 text-center drop-shadow-md">
-        <h2>{instrStep}</h2>
-      </div>
+      <div className="absolute -top-10 -left-10 border-8 border-blue-500 flex items-center justify-center  text-6xl font-bold text-red-800 bg-white rounded-full w-1/6 h-1/6 text-center drop-shadow-md"></div>
       <h3 className="text-xl font-bold">{stepName}</h3>
       {children}
     </div>
--- src/pages/PictureTime.tsx
+++ src/pages/PictureTime.tsx
@@ -3,19 +3,23 @@ import { useState, useEffect } from "react";
 import Instruction from "../components/PicturePage/Instruction";
 import backendApi from "../service/backendApi";
 
+import ShowChosenTeam from "../components/PicturePage/ShowChosenTeam";
+import FinalPicture from "../components/PicturePage/FinalPicture";
+
 import mistyImg from "../assets/Misty.png";
 import ashImg from "../assets/Ash_Ketchum.png";
+import backgroundImg from "../assets/backgrountest.jpg";
 
 type pokeTeam = {
-  id: number;
   name: string;
-  archived: boolean;
   isShiny: boolean;
-  members: number[];
+  members: Array<number>;
+  id: number;
 };
 
 const PictureTime = () => {
   const [teamList, setTeamList] = useState<Array<pokeTeam>>([]);
+  const [chosenTeam, setChosenTeam] = useState<number>(0);
   // get the team names, and team members from the backend API
   async function fetchTeams() {
     try {
@@ -28,17 +32,29 @@ const PictureTime = () => {
   useEffect(() => {
     fetchTeams();
   }, []);
+
+  // listen to which team is being selected
+  const handleTeamChange = (e) => {
+    setChosenTeam(e.currentTarget.value);
+    console.log(chosenTeam);
+  };
+
   //style
   const trainerStyle =
     "h-full w-1/2 p-1 mx-3 rounded-lg bg-blue-200 flex flex-col items-center justify-center";
   const trainerHover = "hover:border-2 hover:border-blue-200 hover:scale-105";
   const trainerTitle = "my-1 font-bold font-xl";
 
+  if (!teamList) {
+    return <div>Nope!</div>;
+  }
   return (
     <div className="h-full flex flex-col">
-      <h1 className="text-center text-5xl my-5">It's pikature time!</h1>
-      <div className="flex h-1/2 w-full  gap-4 justify-center px-10 text-center">
-        <Instruction instrStep={1} stepName="Choose your trainer">
+      <h1 className="text-center text-5xl my-3 text-yellow-500 font-bold">
+        It's pikature time!
+      </h1>
+      <div className="flex h-3/4 w-full my-5  gap-4 justify-center px-10 text-center">
+        <Instruction stepName="1. Choose your trainer">
           <div className="flex m-5 h-3/4 justify-center gap-3">
             <button className={`${trainerStyle} ${trainerHover}`}>
               <img src={ashImg} alt="" className="h-3/4 object-contain" />
@@ -51,13 +67,13 @@ const PictureTime = () => {
           </div>
           <p>You chose Misty!</p>
         </Instruction>
-        <Instruction instrStep={2} stepName="Choose your team">
+        <Instruction stepName="2. Choose your team">
           <div>
-            {teamList.length === 0 ? (
+            {!teamList ? (
               "There are no teams at the moment ..."
             ) : (
-              <select id="teamChange">
-                <option value="-1">Choose a team...</option>
+              <select id="teamChange" onChange={handleTeamChange}>
+                <option value={0}>Choose a team...</option>
                 {teamList.map((team) => {
                   return (
                     <option className="capitalize" value={team.id}>
@@ -68,15 +84,31 @@ const PictureTime = () => {
               </select>
             )}
           </div>
-
-          <p>You chose team 4!</p>
+          {chosenTeam}
+          <ShowChosenTeam pokeTeamId={chosenTeam} teamList={teamList} />
         </Instruction>
-        <Instruction instrStep={3} stepName="Choose your background">
+        <Instruction stepName="3. Choose your background">
           test test
         </Instruction>
       </div>
       <div className="w-full text-center"> Chheeeeeese!</div>
-      <button>Take a picture</button>
+      <div>
+        TEST
+        <FinalPicture
+          chosenTrainer={mistyImg}
+          pokeTeamId={chosenTeam}
+          chosenImg={backgroundImg}
+        />
+        {/* <div className="imageZone relative">
+          <img src={backgroundImg} alt="" />
+          <div className="absolute top-0 flex items-center">
+            {teamList[0].members.map((member: number) => {
+              return <SmallSprite pokeId={Number(member)} shinyState={true} />;
+            })}
+          </div> */}
+        {/* </div>  */}
+        <button>Take a picture</button>
+      </div>
     </div>
   );
 };
